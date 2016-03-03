e7f2699972d1e6f95ec3fc625df1f7592227c37b
Sam Hassell <sam.hassell@agric.wa.gov.au>
2016-03-03 15:39:41 +0800

    remove osfamily and hacks for local repos

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,53 +1,32 @@
-define r::package($r_path = '', $repo = 'http://cran.rstudio.com', $dependencies = false, $timeout = 300) {
+define r::package($r_path = '', $repo = 'http://cran.rstudio.com', $dependencies = false, $timeout = 300, $local = FALSE) {
 
-    case $::osfamily {
-    '^(Debian|RedHat)$': {
+  if $local == TRUE
+  {  
+    $repostring = NULL
+  }   
+  else
+  {   
+    $repostring = "'${repo}'" 
+  }  
 
-      if $r_path == '' {
-        $binary = '/usr/bin/R'
-      }
-      else
-      {
-        $binary = $r_path
-      }
+  if $r_path == '' {
+    $binary = '/usr/bin/R'
+  }
+  else
+  {
+    $binary = $r_path
+  }
 
-      $command = $dependencies ? {
-        true    => "${binary} -e \"install.packages('${name}', repos='${repo}', dependencies = TRUE)\"",
-        default => "${binary} -e \"install.packages('${name}', repos='${repo}', dependencies = FALSE)\""
-      }
+  $command = $dependencies ? {
+    true    => "${binary} -e \"install.packages('${name}', repos=${repostring}, dependencies = TRUE)\"",
+    default => "${binary} -e \"install.packages('${name}', repos=${repostring}, dependencies = FALSE)\""
+  }
 
-      exec { "install_r_package_${name}":
-        command => $command,
-        timeout => $timeout,
-        unless  => "${binary} -q -e \"'${name}' %in% installed.packages()\" | grep 'TRUE'",
-        require => Class['r']
-      }
-
-    }
-    'windows': {
-
-      if $r_path == '' {
-        $binary = 'r.exe'
-      }
-      else
-      {
-        $binary = $r_path
-      }
-
-      $deps = $dependencies ? {
-        true    => 'TRUE',
-        default => 'FALSE'
-      }
-
-      exec { "install_r_package_${name}":
-        command  => template('r/windows_install_rpackage.ps1.erb'),
-        provider => powershell,
-        unless   => template('r/windows_rpackage_check.ps1.erb'),
-        require  => Class['r']
-      }
-
-    }
-    default: { fail("Not supported on osfamily ${::osfamily}") }
+  exec { "install_r_package_${name}":
+    command => $command,
+    timeout => $timeout,
+    unless  => "${binary} -q -e \"'${name}' %in% installed.packages()\" | grep 'TRUE'",
+    require => Class['r']
   }
 
 }
