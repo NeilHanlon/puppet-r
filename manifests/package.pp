286a83a3cb028005ce2ac60bff7082fac46c4cb0
root <root@oraclelinux7.vagrant.vm>
2016-03-10 16:39:21 +1100

    better exit code detection

--- manifests/package.pp
+++ manifests/package.pp
@@ -25,15 +25,16 @@ define r::package($r_path = '', $repo = 'http://cran.rstudio.com', $dependencies
   }
 
   $command = $dependencies ? {
-    true    => "${binary} -e \"install.packages('${name}', repos=${repostring}, dependencies = TRUE)\"",
-    default => "${binary} -e \"install.packages('${name}', repos=${repostring}, dependencies = FALSE)\""
+    true => "echo \"install.packages('${name}', repos=${repostring}, dependencies = TRUE)\" | R CMD BATCH /dev/stdin /dev/stdout | (grep 'non-zero exit status' || exit 0 && exit 1)",
+    default => "echo \"install.packages('${name}', repos=${repostring}, dependencies = FALSE)\" | R CMD BATCH /dev/stdin /dev/stdout | (grep 'non-zero exit status' || exit 0 && exit 1)"
   }
 
   exec { "install_r_package_${name}":
     environment => $environment,
     command => $command,
     timeout => $timeout,
-    unless => "echo \"'#${shortnamestring}' %in% rownames(installed.packages())\" | Rscript --quiet /dev/stdin | grep -v FALSE",
+    path => ["/bin", "/usr/bin", "/usr/sbin", "/sbin"],
+    unless => "echo \"'${shortnamestring}' %in% rownames(installed.packages())\" | Rscript --quiet /dev/stdin | grep -v FALSE",
     creates => $creates,
     require => Class['r']
   }
